e31f973ac4ec0635c932cc2d8d04515796dadc50
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-02-26 14:47:03 +0630

    utc time

--- app/Http/Controllers/Api/V1/game/GameResultController.php
+++ app/Http/Controllers/Api/V1/game/GameResultController.php
@@ -14,14 +14,28 @@ class GameResultController extends Controller
     {
         $operatorCode = Config::get('game.api.operator_code');
         $secretKey = Config::get('game.api.secret_key');        
+        $requestTime = now()->format('YmdHis');
+        $signature = md5($operatorCode . $requestTime . 'gameresult' . $secretKey);
+        
+        // Assuming that the 'Transactions' input is an array of transactions
         $transactions = $request->input('Transactions', []);
+        $currentDateTime = now()->setTimezone('UTC')->format('Y-m-d\TH:i:s.v\Z');
+
+        // Update each transaction's date fields with the current date and time
+        $transactions = array_map(function ($transaction) use ($currentDateTime) {
+            $transaction['SettlementDate'] = $currentDateTime;
+            $transaction['CreatedOn'] = $currentDateTime;
+            $transaction['ModifiedOn'] = $currentDateTime;
+            return $transaction;
+        }, $transactions);
+
         $data = [
             'MemberName' => $request->input('MemberName'),
             'OperatorCode' => $operatorCode,
             'ProductID' => $request->input('ProductID'),
             'MessageID' => $request->input('MessageID'),
-            'RequestTime' => $request->RequestTime,
-            'Sign' => $request->input('Sign'),
+            'RequestTime' => $requestTime,
+            'Sign' => $signature,
             'Transactions' => $transactions
         ];
 
