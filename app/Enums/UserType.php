3b3d607c154308679aaa37b727869e2a2a808075
Tint Aung Khant <aherkersoemin@gmail.com>
2024-03-03 10:52:00 +0700

    feat: add master role to enum

--- app/Enums/UserType.php
+++ app/Enums/UserType.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum UserType: string
 {
     case Admin = "admin";
+    case Master = "master";
     case Agent = "agent";
     case Player = "player";
 
@@ -12,6 +13,7 @@ enum UserType: string
     {
         return match ($this) {
             self::Admin => 10,
+            self::Master => 20,
             self::Agent => 30,
             self::Player => 40,
         };
