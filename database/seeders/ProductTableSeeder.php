99af5af67eeb48f2388ce93e6dc577707bd7e382
sophia <thangneimawi@aceplussolutions.com>
2024-03-06 22:02:33 +0630

    add product image

--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -16,42 +16,50 @@ class ProductTableSeeder extends Seeder
             [
                 'code'  => '1002',
                 'name'  => 'Evolution Gaming',
-                'order' => '1'
+                'order' => '1',
+                'image' => 'evolution_gaming.webp'
             ],
             [
                 'code'  => '1003',
                 'name'  => 'All Bet',
-                'order' => '2'
+                'order' => '2',
+                'image' => 'all_bet.jpeg'
             ],
             [
                 'code'  => '1004',
                 'name'  => 'Big Gaming',
-                'order' => '3'
+                'order' => '3',
+                'image' => 'big_gaming.jpeg'
             ],
             [
                 'code'  => '1005',
                 'name'  => 'SA Gaming',
-                'order' => '4'
+                'order' => '4',
+                'image' => 'sa_gaming.jpeg'
             ],
             [
                 'code'  => '1009',
                 'name'  => 'CQ9',
-                'order' => '6'
+                'order' => '6',
+                'image' => 'cq9.jpeg'
             ],
             [
                 'code'  => '1011',
                 'name'  => 'Play Tech',
-                'order' => '7'
+                'order' => '7',
+                'image' => 'play_tech.png'
             ],
             [
                 'code'  => '1013',
                 'name'  => 'Joker',
-                'order' => '8'
+                'order' => '8',
+                'image' => 'joker.png'
             ],
             [
                 'code'  => '1014',
                 'name'  => 'Dragon Soft',
-                'order' => '9'
+                'order' => '9',
+                'image' => 'dragon_soft.jpeg'
             ],
             [
                 'code'  => '1017',
@@ -61,95 +69,113 @@ class ProductTableSeeder extends Seeder
             [
                 'code'  => '1020',
                 'name'  => 'WM Casino',
-                'order' => '11'
+                'order' => '11',
+                'image' => 'wm_casino.jpeg'
             ],
             [
                 'code'  => '1022',
                 'name'  => 'Sexy Gaming',
-                'order' => '12'
+                'order' => '12',
+                'image' => 'sexy_gaming.jpeg'
             ],
             [
                 'code'  => '1039',
                 'name'  => 'AMAYA',
-                'order' => '13'
+                'order' => '13',
+                'image' => 'amaya.png'
             ],
             [
                 'code'  => '1041',
                 'name'  => 'Habanero',
-                'order' => '14'
+                'order' => '14',
+                'image' => 'habanero.jpeg'
             ],
             [
                 'code'  => '1046',
                 'name'  => 'IBC',
-                'order' => '15'
+                'order' => '15',
+                'image' => 'IBC.png'
             ],
             [
                 'code'  => '1048',
                 'name'  => 'Reevo',
-                'order' => '16'
+                'order' => '16',
+                'image' => 'reevo.jpeg'
             ],
             [
                 'code'  => '1049',
                 'name'  => 'EvoPlay',
-                'order' => '17'
+                'order' => '17',
+                'image' => 'evoplay.png'
             ],
             [
                 'code'  => '1050',
                 'name'  => 'PlayStar',
-                'order' => '18'
+                'order' => '18',
+                'image' => 'playstar.png'
             ],
             [
                 'code'  => '1052',
                 'name'  => 'Dream Gaming',
-                'order' => '19'
+                'order' => '19',
+                'image' => 'dream_gaming.jpeg'
             ],
             [
                 'code'  => '1053',
                 'name'  => 'Nexus 4D',
-                'order' => '20'
+                'order' => '20',
+                'image' => 'nex4d.jpeg'
             ],
             [
                 'code'  => '1074',
                 'name'  => 'HKGP Lottery',
-                'order' => '21'
+                'order' => '21',
+                'image' => 'lottery.jpeg'
             ],
             [
                 'code'  => '1075',
                 'name'  => 'SlotXo',
-                'order' => '22'
+                'order' => '22',
+                'image' => 'slogxo.jpeg'
             ],
             [
                 'code'  => '1076',
                 'name'  => 'AMB Poker',
-                'order' => '23'
+                'order' => '23',
+                'image' => 'ambpoker.webp'
             ],
             [
                 'code'  => '1077',
                 'name'  => 'SkyWind',
-                'order' => '24'
+                'order' => '24',
+                'image' => 'sky_wind.webp'
             ],
             [
                 'code'  => '1081',
                 'name'  => 'BTI',
-                'order' => '25'
+                'order' => '25',
+                'image' => 'BTI.jpeg'
             ],
             [
                 'code'  => '1084',
                 'name'  => 'Advant Play',
-                'order' => '26'
+                'order' => '26',
+                'image' => 'advant_play.jpeg'
             ],
             [
                 'code'  => '1085',
                 'name'  => 'JDB',
-                'order' => '27'
+                'order' => '27',
+                'image' => 'jdb.jpeg'
             ],
             [
                 'code'  => '1091',
                 'name'  => 'Jili',
-                'order' => '28'
+                'order' => '28',
+                'image' => 'jili.png'
             ],
         ];
 
-        Product::insert($data);
+        Product ::insert($data);
     }
 }
