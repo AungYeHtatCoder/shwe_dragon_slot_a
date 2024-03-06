0288e9592ccf31666cc00e4f4f33028464cfaa4b
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-03-07 06:11:52 +0630

    game type getImageAttribute added

--- app/Models/Admin/GameType.php
+++ app/Models/Admin/GameType.php
@@ -9,11 +9,28 @@ class GameType extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['code','name','order'];
+    protected $fillable = ['name','code','order'];
     protected  $appends = ['image'];
 
     public function products()
     {
         return $this->belongsToMany(Product::class,'game_type_product')->withPivot('image');
     }
+
+    public function getImageAttribute()
+    {
+        return $this->products->pluck('pivot.image');
+    }
+     // Define an accessor for the "image" attribute
+    // public function getImageAttribute()
+    // {
+    //     // Return the image attribute from related products or any default logic you prefer
+    //     // For example, if you want to return the first product's image
+    //     if ($this->products->first()) {
+    //         return $this->products->first()->pivot->image;
+    //     }
+
+    //     // Return default image or null if no products are associated
+    //     return null;
+    // }
 }
