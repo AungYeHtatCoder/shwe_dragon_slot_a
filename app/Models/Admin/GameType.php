d1a826a40164f34f50c45506891b46461a5d7571
sophia <thangneimawi@aceplussolutions.com>
2024-03-08 12:37:13 +0630

    add img

--- app/Models/Admin/GameType.php
+++ app/Models/Admin/GameType.php
@@ -9,7 +9,7 @@ class GameType extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name','code','order'];
+    protected $fillable = ['name','code','order','img'];
     protected  $appends = ['image'];
 
     public function products()
