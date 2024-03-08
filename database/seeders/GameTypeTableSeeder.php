531f795bb00cc08bd3da586903ec58763b7347e3
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-03-08 10:49:18 +0630

    game type imags updted

--- database/seeders/GameTypeTableSeeder.php
+++ database/seeders/GameTypeTableSeeder.php
@@ -16,66 +16,79 @@ class GameTypeTableSeeder extends Seeder
             [
                 'name'  => 'Slot',
                 'code'  => '1',
+                'img' => 'slot.png',
                 'order' => '1'
             ],
             [
                 'name'  => 'Live Casino',
                 'code'  => '2',
+                'img' => 'live_casino.png',
                 'order' => '2'
             ],
             [
                 'name'  => 'Sport Book',
                 'code'  => '3',
+                'img' => 'sport_book.png',
                 'order' => '3'
             ],
             [
                 'name'  => 'Virtual Sport',
                 'code'  => '4',
+                'img' => 'virtual_sport.png',
                 'order' => '4'
             ],
             [
                 'name' => 'Lottery',
                 'code' => '5',
+                'img' => 'lottery.png',
                 'order' => '5'
             ],
             [
                 'name' => 'Qipai',
                 'code' => '6',
+                'img' => 'qipai.png',
                 'order' => '6'
             ],
             [
                 'name' => 'P2P',
                 'code' => '7',
+                'img' => 'p2p.png',
                 'order' => '7'
             ],
             [
                 'name' => 'Fishing',
                 'code' => '8',
+                'img' => 'fishing.png',
                 'order' => '8'
             ],
             [
                 'name' => 'Others',
                 'code' => '9',
+                'img' => 'others.png',
                 'order' => ''
             ],
             [
                 'name' => 'Cock Fighting',
                 'code' => '10',
+                'img' => 'cock_fighting.png',
                 'order' => '10'
             ],
             [
                 'name' => 'Bonus',
                 'code' => '11',
+                'img' => 'bonus.png',
                 'order' => '11'
             ],
             [
                 'name' => 'Jackpot',
                 'code' => '12',
+                'img' => 'jackpot.png',
                 'order' => '12'
             ],
             [
                 'name' => 'ESport',
                 'code' => '13',
+                'img' => 'esport.png',
                 'order' => '13'
             ]
             ];
