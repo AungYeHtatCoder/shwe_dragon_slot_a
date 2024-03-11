1858333c8a8121193a0defa42e62452a9291cc51
sophia <thangneimawi@aceplussolutions.com>
2024-03-11 20:46:55 +0630

    edit shedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('make:pull-report')->everySecond();
+        $schedule->command('make:pull-report')->everyFiveSeconds();
     }
 
     /**
