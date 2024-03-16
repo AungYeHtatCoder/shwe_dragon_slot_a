ea4652afbfa3232508392d75dbea42a3e2b0c672
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-03-16 07:37:19 +0630

    production key add

--- config/game.php
+++ config/game.php
@@ -2,8 +2,11 @@
 return [
     'api' => [
         'operator_code' => 'E680',
-        'password'  => 'abcd1234',
-        'secret_key' => 'ljfVJA',
-        'url' => 'https://swmd.6633663.com',
+        // 'password'  => 'abcd1234',
+        // 'secret_key' => 'ljfVJA',
+        // 'url' => 'https://swmd.6633663.com',
+        'password'  => 'abcd1234@E680',
+        'secret_key' => 'MDgXdm',
+        'url' => 'https://prodmd.9977997.com',
     ],
 ];
