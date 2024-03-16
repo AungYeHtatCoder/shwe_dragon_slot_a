d6256fa144cfb61f63c4a66c3e498b76ad8710b6
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-03-16 11:52:44 +0630

    product name added api

--- app/Http/Controllers/Api/V1/Game/GameController.php
+++ app/Http/Controllers/Api/V1/Game/GameController.php
@@ -22,7 +22,7 @@ class GameController extends Controller
 
     public function gameTypeProducts($gameTypeID)
     {
-        $gameTypes = GameType::select('id', 'code')->with(['products' => function ($query) {
+        $gameTypes = GameType::with(['products' => function ($query) {
             $query->orderBy('order', 'desc');
         }])->where('id', $gameTypeID)->where('status',1)
             ->first();
