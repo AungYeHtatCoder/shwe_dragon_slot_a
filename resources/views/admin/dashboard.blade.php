280600bde48daef736c0bd54513c3a05f19a0ecd
Tint Aung Khant <aherkersoemin@gmail.com>
2024-03-23 10:55:02 +0700

    chore: minor changes

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,6 +2,36 @@
 
 @section('content')
     <div class="row">
+        @if ($user->hasRole('Admin'))
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body p-3 position-relative">
+                        <div class="row">
+                            <div class="col text-start">
+                                <p class="text-sm mb-1 text-capitalize font-weight-bold">Provider Balance</p>
+                                <h5 class="font-weight-bolder mb-0">
+                                    {{ number_format($provider_balance, 2) }}MMK
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-body p-3 position-relative">
+                    <div class="row">
+                        <div class="col text-start">
+                            <p class="text-sm mb-1 text-capitalize font-weight-bold">Balance</p>
+                            <h5 class="font-weight-bolder mb-0">
+                                {{ number_format(auth()->user()->balanceFloat, 2) }}MMK
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="col-sm-3">
             <div class="card">
                 <div class="card-body p-3 position-relative">
@@ -22,24 +52,6 @@
                 </div>
             </div>
         </div>
-        @if ($user->hasRole('Admin'))
-            <div class="col-sm-3">
-                <div class="card">
-                    <div class="card-body p-3 position-relative">
-                        <div class="row">
-                            <div class="col text-start">
-                                <p class="text-sm mb-1 text-capitalize font-weight-bold">Provider Balance</p>
-                                <h5 class="font-weight-bolder mb-0">
-                                    {{ number_format($provider_balance, 2) }}MMK
-                                </h5>
-                                <span class="text-sm text-end text-success font-weight-bolder mt-auto mb-0">+55%
-                                    <span class="font-weight-normal text-secondary">since yesterday</span></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endif
         <div class="col-sm-3">
             <div class="card">
                 <div class="card-body p-3 position-relative">
