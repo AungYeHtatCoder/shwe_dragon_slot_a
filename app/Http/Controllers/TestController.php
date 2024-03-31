11ca7d0b3456785d6b8006e9a1e2b9940652a27f
Tint Aung Khant <aherkersoemin@gmail.com>
2024-03-31 18:33:44 +0700

    wip: finical report

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,44 +3,107 @@
 namespace App\Http\Controllers;
 
 use App\Enums\TransactionName;
+use App\Enums\UserType;
+use App\Jobs\UpdateFinicalReportJob;
+use App\Models\FinicalReport;
 use App\Models\SeamlessTransaction;
 use App\Models\User;
+use App\Services\UserTreeService;
 use Bavix\Wallet\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $type = $request->get("type");
+        // $user = User::where("type", UserType::Admin)->first();
 
-        [$from, $to] = match ($type) {
-            "yesterday" => [now()->subDay()->startOfDay(), now()->subDay()->endOfDay()],
-            "this_week" => [now()->startOfWeek(), now()->endOfWeek()],
-            "last_week" => [now()->subWeek()->startOfWeek(), now()->subWeek()->endOfWeek()],
-            default => [now()->subMonth(), now()],
-        };
+        // $child_user = UserType::childUserType($user->type);
 
-        $user = User::find(12);
+        $now = now();
 
-        $transactionQuery = Transaction::join('seamless_transactions', function($join) use($user){
-            $join->on('seamless_transactions.id', '=', 'transactions.seamless_transaction_id')
-                ->whereColumn('seamless_transactions.wager_id', 'wagers.id')
-                ->where("transactions.payable_id", $user->id);
-        })->orderBy('transactions.id', 'desc')
-        ->select('meta->opening_balance')
-        ->limit(1);
+        $users = User::where("type", UserType::Player)->get();
 
-        // return $user->wagers()
-        //     ->where("created_at", ">=", $from)
-        //     ->where("created_at", "<=", $to)
-        //     ->addSelect(['opening_balance' => $transactionQuery])
-        //     ->latest()
-        //     ->get();
+        for ($x = 0; $x <= 31; $x++) {
+            foreach ($users as $user) {
+                $bet_count = rand(0, 20);
 
-        $wagers = $user->wagers()->with(["latestTransaction.transactions" => function($q) use($user){
-            $q->where("payable_id", $user->id);
-        }])->whereBetween("created_at", [$from, $to])
-        ->get();
+                for ($i = 0; $i < $bet_count; $i++) {
+                    $user_tree = UserTreeService::getUserTree($user);
+
+                    $report_date = $this->reportDate($now->clone()->addDays($x));
+
+                    foreach ($user_tree as $user_tree_item) {
+                        if (!$user_tree_item) {
+                            continue;
+                        }
+
+                        $key = $report_date . '_' . $user_tree_item->parent_id;
+                        if (!isset($items[$key])) {
+                            $items[$key] = [
+                                'date' => $report_date,
+                                'user_id' => $user_tree_item->parent_id,
+                                'turnover' => 0,
+                                'payout' => 0,
+                                'win_lose' => 0,
+                                'commission' => 0,
+                            ];
+                        }
+
+                        $amount = (rand(1, 9) * 1000) * 100;
+
+                        if ($bet_count == 0) {
+                            $items[$key]['turnover'] += $amount;
+                        } else {
+                            $luck = rand(1, 10);
+
+                            if ($luck >= 9) {
+                                $items[$key]['payout'] -= $amount;
+                            } else if ($luck >= 7) {
+                                $items[$key]['payout'] += $amount;
+                            } else {
+                                $items[$key]['turnover'] += $amount;
+                            }
+                        }
+
+                        $items[$key]["win_lose"] = $items[$key]["payout"] - $items[$key]["turnover"];
+                    }
+                }
+            }
+
+            $report_dates = collect($items)->pluck('date');
+
+            $existing_report_keys = FinicalReport::whereIn('date', $report_dates)->select(DB::raw('CONCAT(date, "_", user_id) as unique_report_key'))->pluck('unique_report_key')->toArray();
+
+            $new_reports = collect($items)->filter(function ($item) use ($existing_report_keys) {
+                return !in_array($item['date'] . '_' . $item['user_id'], $existing_report_keys);
+            })->values();
+
+            if ($new_reports->count() > 0) {
+                FinicalReport::insert($new_reports->toArray());
+            }
+
+            $update_reports = collect($items)->filter(function ($item) use ($existing_report_keys) {
+                return in_array($item['date'] . '_' . $item['user_id'], $existing_report_keys);
+            })->values();
+
+            foreach ($update_reports as $update_report) {
+                $date = $update_report['date'];
+                $user_id = $update_report['user_id'];
+
+                unset($update_report['date']);
+                unset($update_report['user_id']);
+
+                FinicalReport::where('date', $date)->where('user_id', $user_id)->incrementEach($update_report);
+            }
+        }
+    }
+
+    private function reportDate($date)
+    {
+        $date->setTimezone('Asia/Yangon');
+
+        return $date->format('Y-m-d');
     }
 }
